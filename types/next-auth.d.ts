1fd60e35254799c2c4ffc3ec3c5b562ddf6755ab
nellz16 <149651360+nellz16@users.noreply.github.com>
2025-10-01 18:23:34 +0700

    Add files via upload

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,1 +1,17 @@
+import "next-auth";
 
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    user: {
+      username?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+    username?: string;
+  }
+}
